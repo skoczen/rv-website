af6c34d9ad99eee7351d8a30d868bafac149f7bc
Steven Skoczen <steven@quantumimagery.com>
2019-05-17 10:50:49 -0500

    Script cleanup.

--- bin/bootstrap_server.sh
+++ bin/bootstrap_server.sh
@@ -1,11 +1,17 @@
+#!/bin/sh
 apt update
-do-release-upgrade
 apt install git unzip make python3 python3-pip python3-venv snapd docker-compose -y
 pip3 install --upgrade pip
 
 # Clone codebase
-git clone https://github.com/ronanvenkat/website
+mkdir -p code
+cd code
+# git clone https://github.com/ronanvenkat/website
+git clone https://github.com/skoczen/rv-website.git
 
 # Set up environment
-cd website
+# cd website
+cd rv-website
 touch .env
+
+cd ~
