208f8c083a26231a3b711e9c099cef677338ae63
DanorRon <47954337+DanorRon@users.noreply.github.com>
2019-06-02 14:02:28 -0400

    Added model

--- project/apps/ml_models/mnist.py
+++ project/apps/ml_models/mnist.py
@@ -1,7 +1,25 @@
 import tensorflow as tf
+from tensorflow.keras import layers
+import numpy as np
+import pandas as pd
+import os
+
+def create_model():
+    model = tf.keras.models.Sequential([
+        tf.keras.layers.Flatten(input_shape=(28, 28)),
+        tf.keras.layers.Dense(128, activation='relu'),
+        tf.keras.layers.Dropout(0.2),
+        tf.keras.layers.Dense(10, activation='softmax')
+      ])
+    
+    model.compile(optimizer = 'adam',
+                 loss = 'sparse_categorical_crossentropy',
+                 metrics=['accuracy'])
+    return model
 
 def guess_number(pixel_array):
     
-    return 0
-
-print(tf.__version__)
+    model = create_model()
+    model.load_weights('checkpoint/cp.ckpt')
+    
+    return model.predict(pixel_array)
