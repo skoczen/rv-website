03282bbb5002b01ccf5cc7d99303f5590a97eb9b
DanorRon <47954337+DanorRon@users.noreply.github.com>
2019-06-02 14:08:47 -0400

    Update mnist.py

--- project/apps/ml_models/mnist.py
+++ project/apps/ml_models/mnist.py
@@ -1,25 +1,26 @@
-import tensorflow as tf
-from tensorflow.keras import layers
-import numpy as np
-import pandas as pd
-import os
+#import tensorflow as tf
+#from tensorflow.keras import layers
+#import numpy as np
+#import pandas as pd
+#import os
 
-def create_model():
-    model = tf.keras.models.Sequential([
-        tf.keras.layers.Flatten(input_shape=(28, 28)),
-        tf.keras.layers.Dense(128, activation='relu'),
-        tf.keras.layers.Dropout(0.2),
-        tf.keras.layers.Dense(10, activation='softmax')
-      ])
+#def create_model():
+#    model = tf.keras.models.Sequential([
+#        tf.keras.layers.Flatten(input_shape=(28, 28)),
+#        tf.keras.layers.Dense(128, activation='relu'),
+#        tf.keras.layers.Dropout(0.2),
+#        tf.keras.layers.Dense(10, activation='softmax')
+#      ])
     
-    model.compile(optimizer = 'adam',
+#    model.compile(optimizer = 'adam',
                  loss = 'sparse_categorical_crossentropy',
                  metrics=['accuracy'])
-    return model
+#    return model
 
 def guess_number(pixel_array):
     
-    model = create_model()
-    model.load_weights('checkpoint/cp.ckpt')
+#    model = create_model()
+#    model.load_weights('checkpoint/cp.ckpt')
     
-    return model.predict(pixel_array)
+#    return model.predict(pixel_array)
+    return 0
