6ba80657a1fce60de92583d071df14555269c132
Steven Skoczen <steven@quantumimagery.com>
2019-06-08 19:57:26 +0200

    Adds image inversion.

--- project/apps/image_reader/helpers.py
+++ project/apps/image_reader/helpers.py
@@ -25,7 +25,8 @@ def turn_small_image_into_array(small_image):
     Takes a 28x28 image, and returns a 28x28 array of brightness values (0-1)
     """
     converted_image = small_image.convert("L")
-    pixel_array = numpy.array(converted_image)
+    inverted_image = ImageOps.invert(converted_image)
+    pixel_array = numpy.array(inverted_image)
     return pixel_array
 
 
